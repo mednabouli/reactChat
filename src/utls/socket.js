ca0141b611f2ad3271bdee4890467ea237eb25d6
Bunks18 <wbunker11@gmail.com>
2017-09-29 08:49:05 -0400

    channel attempt

--- src/utls/socket.js
+++ src/utls/socket.js
@@ -1,4 +1,4 @@
 import io from 'socket.io-client';
 
-export const socket = io("https://fierce-woodland-36564.herokuapp.com");
-// export const socket = io("http://localhost:8000");
+// export const socket = io("https://fierce-woodland-36564.herokuapp.com");
+export const socket = io("http://localhost:8000");
